00f363001942014b5389c46f73355e8adfabb116
RichardJohnRx <the.cool.guy492@gmail.com>
2019-11-07 07:34:10 -0600

    Se añadió REST de varios modulos

--- src/java/com/verum/spa/restServices/ApplicationConfig.java
+++ src/java/com/verum/spa/restServices/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.verum.spa.restServices.BranchRest.class);
         resources.add(com.verum.spa.restServices.GenericResource.class);
         resources.add(com.verum.spa.restServices.ProductRest.class);
         resources.add(com.verum.spa.restServices.RoomRest.class);
